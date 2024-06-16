efa0b93ec4f2f3a7443a317567386bb103f776d1
Mufei Li <mufeili1996@gmail.com>
2024-06-16 04:07:36 +0000

    update

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
+import torch
+
 def main(args):
-    pass
+    torch.set_num_threads(args.num_threads)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
+    parser.add_argument("--num_threads", type=int, default=16)
     args = parser.parse_args()
 
     main(args)
