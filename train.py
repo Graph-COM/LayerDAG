4d6af3cefe75a982677600deecb14a24e6a72512
Mufei Li <mufeili1996@gmail.com>
2024-06-16 04:01:42 +0000

    update

--- train.py
+++ train.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
+def main(args):
     pass
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    args = parser.parse_args()
+
+    main(args)
