306079229876e4055cac5720cc8e95d54f09409d
Mufei Li <mufeili1996@gmail.com>
2024-06-16 04:08:37 +0000

    update

--- train.py
+++ train.py
@@ -3,6 +3,9 @@ import torch
 def main(args):
     torch.set_num_threads(args.num_threads)
 
+    device_str = "cuda:0" if torch.cuda.is_available() else "cpu"
+    device = torch.device(device_str)
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
