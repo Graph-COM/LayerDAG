a2d46d48a3f28d4251d81393ef19407712764b6c
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-22 17:07:00 -0400

    update

--- src/dataset/tpu_tile.py
+++ src/dataset/tpu_tile.py
@@ -6,3 +6,7 @@ from .general import DAGDataset
 def get_tpu_tile():
     root_path = os.path.dirname(os.path.abspath(__file__))
     root_path = os.path.join(root_path, '../../data_files/tpu_tile_processed')
+
+    train_path = os.path.join(root_path, 'train.pth')
+    val_path = os.path.join(root_path, 'val.pth')
+    test_path = os.path.join(root_path, 'test.pth')
