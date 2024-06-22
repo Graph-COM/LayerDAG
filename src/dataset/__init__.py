692749e9a7339e4a77d3392f9a144d7a229b47fd
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-22 17:00:01 -0400

    update

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,7 +1,8 @@
 from .general import DAGDataset
+from .tpu_tile import get_tpu_tile
 
 def load_dataset(dataset_name):
     if dataset_name == 'tpu_tile':
-        pass
+        return get_tpu_tile()
     else:
         return NotImplementedError
--- src/dataset/tpu_tile.py
+++ src/dataset/tpu_tile.py
@@ -2,3 +2,6 @@ import os
 import torch
 
 from .general import DAGDataset
+
+def get_tpu_tile():
+    pass
