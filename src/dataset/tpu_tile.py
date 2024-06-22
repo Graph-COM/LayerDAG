2a2f6db757151cfb4986c9a01ae7b90c7d6fb62d
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-22 17:02:46 -0400

    update

--- src/dataset/tpu_tile.py
+++ src/dataset/tpu_tile.py
@@ -4,4 +4,5 @@ import torch
 from .general import DAGDataset
 
 def get_tpu_tile():
-    pass
+    root_path = os.path.dirname(os.path.abspath(__file__))
+    root_path = os.path.join(root_path, '../../data_files/tpu_tile_processed')
