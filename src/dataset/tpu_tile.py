67a462508a6d064eb4c85740f02ee4fdbdbf3994
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-22 17:12:41 -0400

    update

--- src/dataset/tpu_tile.py
+++ src/dataset/tpu_tile.py
@@ -12,3 +12,8 @@ def get_tpu_tile():
     test_path = os.path.join(root_path, 'test.pth')
 
     print('Loading TPU Tile dataset...')
+    # Load the pre-processed TPU Tile dataset, where for each kernel graph, we
+    # average the normalized runtime over multiple compiler configurations.
+    train_set = torch.load(train_path)
+    val_set = torch.load(val_path)
+    test_set = torch.load(test_path)
