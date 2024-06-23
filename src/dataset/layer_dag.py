b5abc4b76550267bf1c506868e046acc21a10429
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-22 23:31:48 -0400

    update

---
+++ src/dataset/layer_dag.py
@@ -0,0 +1,58 @@
+import torch
+
+from collections import defaultdict
+from torch.utils.data import Dataset
+
+__all__ = []
+
+class LayerDAGBaseDataset(Dataset):
+    def __init__(self, conditional=False):
+        self.input_src = []
+        self.input_dst = []
+        self.input_x_n = []
+        self.input_level = []
+
+        self.input_e_start = []
+        self.input_e_end = []
+        self.input_n_start = []
+        self.input_n_end = []
+
+        self.conditional = conditional
+        if conditional:
+            self.input_y = []
+            self.input_g = []
+
+    def get_in_deg(self, dst, num_nodes):
+        return torch.bincount(dst, minlength=num_nodes).tolist()
+
+    def get_out_adj_list(self, src, dst):
+        out_adj_list = defaultdict(list)
+        num_edges = len(src)
+        for i in range(num_edges):
+            out_adj_list[src[i]].append(dst[i])
+        return out_adj_list
+
+    def get_in_adj_list(self, src, dst):
+        in_adj_list = defaultdict(list)
+        num_edges = len(src)
+        for i in range(num_edges):
+            in_adj_list[dst[i]].append(src[i])
+        return in_adj_list
+
+    def base_postprocess(self):
+        self.input_src = torch.LongTensor(self.input_src)
+        self.input_dst = torch.LongTensor(self.input_dst)
+
+        # Case1: self.input_x_n[0] is an int.
+        # Case2: self.input_x_n[0] is a tensor of shape (F).
+        self.input_x_n = torch.LongTensor(self.input_x_n)
+        self.input_level = torch.LongTensor(self.input_level)
+
+        self.input_e_start = torch.LongTensor(self.input_e_start)
+        self.input_e_end = torch.LongTensor(self.input_e_end)
+        self.input_n_start = torch.LongTensor(self.input_n_start)
+        self.input_n_end = torch.LongTensor(self.input_n_end)
+
+        if self.conditional:
+            self.input_y = torch.tensor(self.input_y)
+            self.input_g = torch.LongTensor(self.input_g)
