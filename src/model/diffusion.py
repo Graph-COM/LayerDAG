b2da394855126156a78237a416f7ba9deebbaf1c
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-23 17:32:56 -0400

    update

--- src/model/diffusion.py
+++ src/model/diffusion.py
@@ -5,5 +5,16 @@ __all__ = [
 ]
 
 class DiscreteDiffusion(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 marginal_list,
+                 T,
+                 s=0.008):
+        """
+        Parameters
+        ----------
+        marginal_list : list of torch.Tensor
+            marginal_list[f] is the marginal distribution of the f-th attribute
+        s : float
+            Constant in noise schedule
+        """
         super().__init__()
