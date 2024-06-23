37f063ff3e54984a94a4e04fa8adc43fc084f71e
Li <mli788@panli-srv2.ece.gatech.edu>
2024-06-23 17:50:14 -0400

    update

--- src/model/diffusion.py
+++ src/model/diffusion.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -37,5 +38,23 @@ class DiscreteDiffusion(nn.Module):
             self.m_list.append(nn.Parameter(marginal_f, requires_grad=False))
 
         self.T = T
-        import ipdb
-        ipdb.set_trace()
+        # Cosine schedule as proposed in
+        # https://arxiv.org/abs/2102.09672
+        num_steps = T + 2
+        t = np.linspace(0, num_steps, num_steps)
+        # Schedule for \bar{alpha}_t = alpha_1 * ... * alpha_t
+        alpha_bars = np.cos(0.5 * np.pi * ((t / num_steps) + s) / (1 + s)) ** 2
+        # Make the largest value 1.
+        alpha_bars = alpha_bars / alpha_bars[0]
+        alphas = alpha_bars[1:] / alpha_bars[:-1]
+
+        self.betas = torch.from_numpy(1 - alphas).float()
+        self.alphas = 1 - torch.clamp(self.betas, min=0, max=0.9999)
+
+        log_alphas = torch.log(self.alphas)
+        log_alpha_bars = torch.cumsum(log_alphas, dim=0)
+        self.alpha_bars = torch.exp(log_alpha_bars)
+
+        self.betas = nn.Parameter(self.betas, requires_grad=False)
+        self.alphas = nn.Parameter(self.alphas, requires_grad=False)
+        self.alpha_bars = nn.Parameter(self.alpha_bars, requires_grad=False)
